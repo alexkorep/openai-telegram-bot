62b7418edcf462cd69823c3b4bda209c499677da
Alexey Korepanov <alexkorep@gmail.com>
2024-06-15 08:20:51 +0200

    Change model to gpt4-o

--- handle_text.py
+++ handle_text.py
@@ -6,7 +6,8 @@ from consts import OPENAI_REQUEST_TIMEOUT
 HISTORY_LEN = 128
 # Model to use
 #MODEL_NAME = "gpt-3.5-turbo"
-MODEL_NAME = "gpt-4-0125-preview"
+#MODEL_NAME = "gpt-4-0125-preview"
+MODEL_NAME = "gpt-4o"
 # Token limit for the model
 MODEL_TOKEN_LIMIT = 4096
 # How many tokens we reserve for the history. That means that
